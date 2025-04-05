76f454b46353fe94a24ee6f7724b0614cdc89079
AlekDiaz <127565159+AlekDiaz@users.noreply.github.com>
2025-04-04 20:25:18 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,39 +1,11 @@
 import streamlit as st
-import gspread
-import pandas as pd
-from google.oauth2.service_account import Credentials
+import Sheets
 
-# Cargar credenciales desde Streamlit secrets
-creds_dict = st.secrets["gcp_service_account"]
+# Testing info
+st.write("### 📊 Vista previa de los datos")
+df = Sheets.get_table('Nodol')
+st.dataframe(df)  # Display df
 
-# Definir los scopes correctos
-scopes = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
-
-# Crear credenciales con los scopes
-creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
-
-# Autorizar con gspread
-gc = gspread.authorize(creds)
-
-# Intentar abrir la hoja de cálculo
-try:
-    spreadsheet = gc.open("Formula")
-    sheet = spreadsheet.sheet1  # Usa la primera hoja (puedes cambiarlo si tienes varias)
-    data = sheet.get_all_records()  # Obtener todos los datos como lista de diccionarios
-    df = pd.DataFrame(data)  # Convertir a DataFrame de pandas
-    
-    st.success("✅ ¡Conexión exitosa a Google Sheets!")
-
-    # Mostrar las primeras filas de la tabla
-    st.write("### 📊 Vista previa de los datos")
-    st.dataframe(df)  # Mostrar tabla interactiva en Streamlit
-
-    # Mostrar nombres de las columnas
-    st.write("### 🏷️ Columnas disponibles:")
-    st.write(df.columns.tolist())
-
-except Exception as e:
-    st.error(f"❌ Error al conectar con Google Sheets: {e}")
+# df properties
+st.write("### 🏷️ Columnas disponibles:")
+st.write(df.columns.tolist())
---
+++ Sheets.py
@@ -0,0 +1,32 @@
+import gspread
+import pandas as pd
+from google.oauth2.service_account import Credentials
+from streamlit import secrets
+
+# Streamlit Creds
+creds_dict = secrets["gcp_service_account"]
+
+# Direct Access, scopes required (Sheets & Drive) 
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# OAuth2 creds for service accounts
+creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
+
+# Authorize gspread
+gc = gspread.authorize(creds)
+
+# Main search function
+def get_table(key: str) -> pd.DataFrame:
+    try:
+        spreadsheet = gc.open('Formulas')
+        sheet = spreadsheet.worksheet(key)  # First Sheets
+        data = sheet.get_all_records()  # Get all data **[{}{}]**
+        df = pd.DataFrame(data)  # Cast data to Pandas DataFrame
+
+        return df
+
+    except Exception as e:
+        return False
