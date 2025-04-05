0ebcaba46b7a5739c1d9b99afdd35187a53e1828
AlekDiaz <127565159+AlekDiaz@users.noreply.github.com>
2025-04-04 22:08:43 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -3,10 +3,6 @@ import Sheets
 
 # Testing info
 st.write("### 📊 Vista previa de los datos")
-df = Sheets.get_table('Nodol')
-
-# df properties
-st.write("### 🏷️ Columnas disponibles:")
-st.write(df.columns.tolist())
+df = Sheets.get_table()
 
 selected_sheet = st.selectbox("Selecciona una pestaña 📄", Sheets.get_titles(df))
