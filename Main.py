79e3fb6a1691cc6fe05f882a23cbcdeba75a08f3
AlekDiaz <127565159+AlekDiaz@users.noreply.github.com>
2025-04-04 20:28:37 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -3,9 +3,13 @@ import Sheets
 
 # Testing info
 st.write("### 📊 Vista previa de los datos")
-df = Sheets.get_table('Nodol')
-st.dataframe(df)  # Display df
 
-# df properties
-st.write("### 🏷️ Columnas disponibles:")
-st.write(df.columns.tolist())
+if not Sheets.get_table('Nodol'):
+    st.write("La tabla no se pudo encontrar")
+else:
+    df = Sheets.get_table('Nodol')
+    st.dataframe(df)  # Display df
+
+    # df properties
+    st.write("### 🏷️ Columnas disponibles:")
+    st.write(df.columns.tolist())
